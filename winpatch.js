f343b6251ee5ebe2df5c6eb23347186510d76664
David N <david.nefdt@gmail.com>
2025-09-23 04:25:17 +0200

    commit6

--- winpatch.js
+++ winpatch.js
@@ -1,7 +1,7 @@
 /**
  * @description MeshCentral Windows Patch Management Plugin (force button visible)
  * @license Apache-2.0
- * @version v0.0.5
+ * @version v0.0.6
  */
 
 "use strict";
@@ -18,21 +18,44 @@ module.exports.winpatch = function (parent) {
     // Called whenever a device page refreshes
     obj.onDeviceRefreshEnd = function () {
     try {
-        alert("WinPatch hook fired, about to add button");
+        alert("WinPatch hook fired, searching for iframe...");
 
-        var btn = document.createElement('input');
-        btn.type = 'button';
-        btn.id = 'winpatchBtn';
-        btn.value = 'HELLO WORLD BUTTON';
-        btn.onclick = function () { alert('Button clicked!'); };
+        // Find all iframes
+        var iframes = document.getElementsByTagName('iframe');
+        var targetDoc = null;
 
-        document.body.appendChild(btn);
+        for (var i = 0; i < iframes.length; i++) {
+            try {
+                // Look for iframe that has the device page inside
+                var doc = iframes[i].contentDocument || iframes[i].contentWindow.document;
+                if (doc && doc.getElementById('d2devEvent')) {
+                    targetDoc = doc;
+                    break;
+                }
+            } catch (e) { }
+        }
 
-        alert("Button appended to body");
+        if (!targetDoc) {
+            alert("Could not find device iframe");
+            return;
+        }
+
+        // Now inject button into that iframe's body
+        if (!targetDoc.getElementById('winpatchBtn')) {
+            var btn = targetDoc.createElement('input');
+            btn.type = 'button';
+            btn.id = 'winpatchBtn';
+            btn.value = 'HELLO WORLD BUTTON';
+            btn.onclick = function () { alert('Button inside iframe clicked!'); };
+
+            targetDoc.body.appendChild(btn);
+            alert("Button appended inside iframe body");
+        }
     } catch (err) {
         alert("Error: " + err.toString());
     }
 };
+
 
     return obj;
 };
