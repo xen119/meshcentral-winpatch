00f1b5c8bab16b3b0108dc2af48f09fe1d9dabe2
David N <david.nefdt@gmail.com>
2025-09-23 04:28:17 +0200

    commit7

--- winpatch.js
+++ winpatch.js
@@ -1,7 +1,7 @@
 /**
  * @description MeshCentral Windows Patch Management Plugin (force button visible)
  * @license Apache-2.0
- * @version v0.0.6
+ * @version v0.0.7
  */
 
 "use strict";
@@ -18,43 +18,34 @@ module.exports.winpatch = function (parent) {
     // Called whenever a device page refreshes
     obj.onDeviceRefreshEnd = function () {
     try {
-        alert("WinPatch hook fired, searching for iframe...");
+        alert("WinPatch hook fired, scanning DOM...");
 
-        // Find all iframes
-        var iframes = document.getElementsByTagName('iframe');
-        var targetDoc = null;
-
-        for (var i = 0; i < iframes.length; i++) {
-            try {
-                // Look for iframe that has the device page inside
-                var doc = iframes[i].contentDocument || iframes[i].contentWindow.document;
-                if (doc && doc.getElementById('d2devEvent')) {
-                    targetDoc = doc;
-                    break;
-                }
-            } catch (e) { }
+        // Dump IDs of all divs
+        var divs = document.getElementsByTagName('div');
+        var ids = [];
+        for (var i = 0; i < divs.length; i++) {
+            if (divs[i].id) ids.push(divs[i].id);
         }
+        alert("Found div IDs: " + ids.join(", "));
 
-        if (!targetDoc) {
-            alert("Could not find device iframe");
-            return;
-        }
-
-        // Now inject button into that iframe's body
-        if (!targetDoc.getElementById('winpatchBtn')) {
-            var btn = targetDoc.createElement('input');
-            btn.type = 'button';
-            btn.id = 'winpatchBtn';
-            btn.value = 'HELLO WORLD BUTTON';
-            btn.onclick = function () { alert('Button inside iframe clicked!'); };
-
-            targetDoc.body.appendChild(btn);
-            alert("Button appended inside iframe body");
+        // Try to inject button into the first found container
+        if (divs.length > 0) {
+            var target = divs[0];
+            if (!document.getElementById('winpatchBtn')) {
+                var btn = document.createElement('input');
+                btn.type = 'button';
+                btn.id = 'winpatchBtn';
+                btn.value = 'HELLO WORLD BUTTON';
+                btn.onclick = function () { alert('Button clicked!'); };
+                target.appendChild(btn);
+                alert("Button appended to div id=" + target.id);
+            }
         }
     } catch (err) {
         alert("Error: " + err.toString());
     }
 };
+
 
 
     return obj;
